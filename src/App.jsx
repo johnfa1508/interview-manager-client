954df543af873eb79bb842eab28b1abbcaf4c4f2
John Ferdie Abueg <john.abueg@gmail.com>
2024-11-08 14:37:19 +0100

    working modal test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
+import ModalTesting from './components/testing/ModalTest';
+import useModal from './hooks/useModal';
 
 function App() {
+  const { openModal, setModal } = useModal();
+  const showModal = () => {
+    // This function will be implemented in the next step
+    setModal('Modal testing', <ModalTesting />);
+    openModal();
+  };
+
   return (
     <>
       <h1>Interview Manager</h1>
+      <button onClick={showModal}>Test modal</button>
     </>
   );
 }
---
+++ src/components/modal/style.jsx
@@ -0,0 +1,13 @@
+const style = {
+  content: {
+    margin: '0 auto',
+    position: 'relative',
+    top: '14%',
+    backgroundColor: 'gray'
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)'
+  }
+};
+
+export default style;
