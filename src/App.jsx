0088d7cc238fd955b83132c272196ae941620e71
John Ferdie Abueg <john.abueg@gmail.com>
2024-11-12 10:07:35 +0100

    Header, nav, dashboard structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Logo from './Icons/Logo';
+import Dashboard from './pages/dashboard';
 
 function App() {
   return (
     <>
-      <Logo textColor={"#FFFFFF"} arrowColor={"#030311"} boxColor={"#20b2aa"}/>
+      <Dashboard />
     </>
   );
 }
---
+++ src/components/interviewDashboard/index.jsx
@@ -0,0 +1,8 @@
+export default function InterviewDashboard() {
+  return (
+    <div>
+      <h1>Search bar here</h1>
+      <h2>Interviews here</h2>
+    </div>
+  );
+}
