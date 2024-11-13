56f11f7d93572888b2ae0622ca5579eba719e3e3
Magnus Brandsegg <magbra00@gmail.com>
2024-11-13 09:07:31 +0100

    Add roting and contruction pages for archive, profile and logbook

---
+++ src/pages/logbook/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+export default function LogbookPage() {
+  return (
+    <div className="construction-container">
+      <h1>LOGBOOK PAGE IN THE WORKS</h1>
+      <div className="construction-animation">
+        🚧👷‍♂️ Under Construction 👷‍♀️🚧
+      </div>
+    </div>
+  );
+}
