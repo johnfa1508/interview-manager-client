6a5f0a43c769cd4955c8410a3f51e73783464cc2
John Ferdie Abueg <john.abueg@gmail.com>
2024-11-13 10:36:41 +0100

    moved modal testing to interview columns

--- src/components/interviewDashboardColumn/index.jsx
+++ src/components/interviewDashboardColumn/index.jsx
@@ -2,16 +2,27 @@
 import { Droppable } from '../droppable';
 import { Draggable } from '../draggable';
 import { formatDateTime } from '../../service/formatDate';
+import useModal from '../../hooks/useModal';
+import ViewInterviewModal from '../ViewInterviewModal';
 import './style.css';
 
 export default function InterviewColumn({ id, interviews, interviewContainer, searchValue }) {
+  const { openModal, setModal } = useModal();
+
   const filteredInterviews = interviewContainer[id].filter((itemId) =>
     interviews.some((interview) => interview.InterviewId === itemId)
   );
 
+  const showModal = () => {
+    // This function will be implemented in the next step
+    setModal(<ViewInterviewModal />);
+    openModal();
+  };
+
   return (
     <div className="column">
       <h3>{id}</h3>
+      <button onClick={showModal}>Show modal</button>
       <Droppable id={id}>
         {filteredInterviews.map((itemId, index) => {
           const interview = interviews.find((interview) => interview.InterviewId === itemId);
