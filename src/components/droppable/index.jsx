ac1644fdba873cc58eb1e34cb274e3a97b8dbb9e
John Ferdie Abueg <john.abueg@gmail.com>
2024-11-28 11:26:42 +0100

    styling for droppable/draggable containers

--- src/components/droppable/index.jsx
+++ src/components/droppable/index.jsx
@@ -6,13 +6,18 @@ export function Droppable(props) {
   const { isOver, setNodeRef } = useDroppable({
     id: props.id
   });
+
+  const rootStyle = getComputedStyle(document.documentElement);
+  const backgroundColor = isOver ? rootStyle.getPropertyValue('--background-color') : '';
+
   const style = {
     border: isOver ? '2px dashed green' : '2px dashed grey',
     padding: '20px',
     borderRadius: '8px',
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '400px'
+    minHeight: '400px',
+    backgroundColor: backgroundColor
   };
 
   return (
