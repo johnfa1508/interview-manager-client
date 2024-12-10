cd62978e43e5ab2aad3fd35d239266235b3bd96a
John Ferdie Abueg <john.abueg@gmail.com>
2024-12-10 09:20:45 +0100

    profile dropdown close when clicking outside dropdown

--- src/components/profileDropdown/index.jsx
+++ src/components/profileDropdown/index.jsx
@@ -1,18 +1,32 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import ProfileImage from '../ProfileImage';
 import './style.css';
 
 export default function ProfileDropdown({ image, onLogout }) {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="profile-dropdown-container">
+    <div className="profile-dropdown-container" ref={dropdownRef}>
       <button type="button" className="profile-dropdown-button" onClick={toggleDropdown}>
         <ProfileImage image={image} size="60px" />
       </button>
