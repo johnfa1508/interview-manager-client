c1c581deed17d04ca8f0fb879cb9e678e830cca3
John Ferdie Abueg <john.abueg@gmail.com>
2024-12-11 09:23:19 +0100

    deleted unused positionedmodal

--- src/hooks/usePositionedModal.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { PositionedModalContext } from '../context/positionedModal';
-
-const usePositionedModal = () => {
-  return useContext(PositionedModalContext);
-};
-
-export default usePositionedModal;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { ModalProvider } from './context/modal.jsx';
-import { PositionedModalProvider } from './context/positionedModal.jsx';
 import Modal from './components/modal/index.jsx';
-import PositionedModal from './components/positionedModal/index.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/auth.jsx';
 
@@ -14,11 +12,8 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <ModalProvider>
-          <PositionedModalProvider>
-            <App />
-            <Modal />
-            <PositionedModal />
-          </PositionedModalProvider>
+          <App />
+          <Modal />
         </ModalProvider>
       </AuthProvider>
     </BrowserRouter>
