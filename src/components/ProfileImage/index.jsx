ac202cb074e799317c8aac253f51611c3e423436
John Ferdie Abueg <john.abueg@gmail.com>
2024-12-13 09:02:11 +0100

    default image for users without image

--- src/components/ProfileImage/index.jsx
+++ src/components/ProfileImage/index.jsx
@@ -12,13 +12,12 @@ const ProfileImage = ({ image, size = '130px' }) => {
           style={{ width: size, height: size }}
         />
       ) : (
-        <div
-          /*CHANGE THIS WITH DEFAULT PRPOFILE PIC*/
-          className="profile-image-placeholder"
+        <img
+          src={'src/images/image1.jpg'}
+          alt="Profile"
+          className="profile-image"
           style={{ width: size, height: size }}
-        >
-          No Image
-        </div>
+        />
       )}
     </div>
   );
